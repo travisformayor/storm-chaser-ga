b58bcc0d429729b0f54edf3d9caf505b92b27ef5
Teresa Cho <tcho@cca.edu>
2019-04-26 11:04:50 -0700

    fixed the routes and added user list, connected validation errors

--- public/js/users.js
+++ public/js/users.js
@@ -1,26 +1,31 @@
 // Get all pets info
-const api = '/api/users';
-const request = {
-  method: 'GET',
-  url: api,
-  success: handleSuccess,
-  error: handleError
-};
-$.ajax(request);
-
-function handleError(err) {
-  console.log(err);
-};
-function handleSuccess(res) {
-  if (res.length > 0) {
-    res.forEach(user => {
-      let userHtml = `
-        <tr>
-          <td>${user.firstName}</td>
-          <td>${user.lastName}</td>
-          <td>${user.email}</td>
-        </tr>`;
-      $('#userList').append(userHtml);
-    });
+if ($('#userList').length) {
+  const api = '/api/users';
+  const request = {
+    method: 'GET',
+    url: api,
+    success: handleSuccess,
+    error: handleError
   };
-};
+  $.ajax(request);
+  
+  function handleError(err) {
+    console.log(err);
+  };
+  function handleSuccess(res) {
+    if (res.length > 0) {
+      let counter = 1;
+      res.forEach(user => {
+        let userHtml = `
+          <tr> 
+            <td>${counter}</td>
+            <td>${user.firstName}</td>
+            <td>${user.lastName}</td>
+            <td>${user.email}</td>
+          </tr>`;
+        $('#userList').append(userHtml);
+        counter++;
+      });
+    };
+  };
+}
--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.get('/', (req, res) => {
 
 //GET All Users
 app.get('/api/users', (req, res) => {
-  db.Users.find()
-  .exec((er, AllUsers) => {
+  db.User.find()
+  .exec((err, allUsers) => {
     if (err) return res.json({error: err});
     res.json(allUsers);
   })
@@ -49,54 +49,79 @@ app.get('/signup', (req, res) => {
   res.render('landing');
 });
 
+//GET User Route
+app.get('/users', (req, res) => {
+  res.render('users');
+})
+
 //POST Create User Route
 app.post('/signup', (req, res) => {
   const errors = [];
 
   //Validation Form Data
-  if(!req.body.name) {
-    errors.push({message: 'Please enter your name'});
-  } else if (req.body.name) {
+  if(!req.body.firstName) {
+    errors.push({
+      type: 'firstName',
+      message: 'Please enter your first name'
+    });
+  } else if (req.body.firstName) {
     let check = /[A-Za-z]+$/;
-    if(req.body.name.match(check)){
+    // console.log(req.body.firstName)
+    // console.log(req.body.firstName.length)
+    if(req.body.firstName.match(check) && req.body.firstName.length >= 2){
     } else {
-      errors.push({message: 'Please enter a valid first name'})
+      errors.push({
+        type: 'firstName',
+        message: 'Please enter a valid first name'
+      })
     }
   }
 
-  if(!req.body.last) {
-    errors.push({message: 'Please enter your last name'})
-  } else if (req.body.last) {
+  if(!req.body.lastName) {
+    errors.push({
+      type: 'lastName',
+      message: 'Please enter your last name'
+    })
+  } else if (req.body.lastName) {
     let check = /[A-Za-z]+$/;
-    if(req.body.last.match(check)){
+    if(req.body.lastName.match(check) && req.body.lastName.length >= 2){
     } else {
-      errors.push({message: 'Please enter a valid last name'})
+      errors.push({
+        type: 'lastName',
+        message: 'Please enter a valid last name'
+      })
     }
   }
 
   if(!req.body.email) {
-    errors.push({message: 'Please enter your email'});
+    errors.push({
+      type: 'email',
+      message: 'Please enter your email'
+    });
   } else if (req.body.email) {
     let check = /\S+@\S+\.\S+/;
     if(req.body.email.match(check)){
     } else {
-      errors.push({message: 'Please enter a valid email'})
+      errors.push({
+        type: 'email',
+        message: 'Please enter a valid email'
+      })
     }
   }
 
   //If there are any validation errors, Re-render signup page with error messages
   if(errors.length) {
     return res.json({
-      error: [errors]
+      errors: errors
     });
   };
   //Add User to DB, catch and then-res JSON success, send success
-  // db.User.create(req.body, (err, newUser) => {
-  //   .catch(err => res.json({error: err}))
-  //   .then(res.json(success))
-  // })
-
+  console.log(req.body)
+  db.User.create(req.body)
+    .catch(err => res.json({error: err}))
+    .then(newUser => res.json({success: 'success'}))
 })
+
 
 
 // Server ======================================= //
